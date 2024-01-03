3d109bef9a625f9ce72ad2bff071ef8e1ae7b046
hastrotech1 <hastrotech1@gmail.com>
2024-01-03 15:45:36 +0100

    fixed some bugs

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+
+#str_concat_1 = "Holberton"
+#str_concat_2 = "School"
+#str_concat_1 += " " + str_concat_2
+#print("Welcome to {}!".format(str_concat_1))
+
 str_concat_1 = "Holberton"
-str_concat_2 = "School"
-str_concat_1 += " " + str_concat_2
-print("Welcome to {}!".format(str_concat_1))
+str_conct_2 = "School"
+str_concat_1 = f"{str_concat_1} {str_concat_2}"
+print(f"Welcome to {str1}!")
