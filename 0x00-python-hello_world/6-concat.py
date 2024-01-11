e08491d93aeb0f947b44b7e4396c41a1b1eb3c7c
hastrotech1 <hastrotech1@gmail.com>
2024-01-11 21:46:36 +0100

    updated task 6

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-
-#str_concat_1 = "Holberton"
-#str_concat_2 = "School"
-#str_concat_1 += " " + str_concat_2
-#print("Welcome to {}!".format(str_concat_1))
-
 str1 = "Holberton"
 str2 = "School"
 str1 = f"{str1} {str2}"
