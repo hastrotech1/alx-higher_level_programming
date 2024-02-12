3f49536eac4d08df277c23c644f4a6f9aeb88dd0
hastrotech1 <hastrotech1@gmail.com>
2024-02-12 10:01:06 +0100

    Adjusted my code the fourth time to execute the right output of the last digit of numbers in tens-thousands

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 LastDig = number % 10
-#LastDig still means Last Digit fam
+# LastDig still means Last Digit fam
 if LastDig > 5:
     print(f"Last digit of {number} is {LastDig} and is greater than 5")
 elif LastDig == 0:
