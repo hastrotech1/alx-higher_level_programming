ef285735a0fbe56747bdec857555c1424d7a081a
hastrotech1 <hastrotech1@gmail.com>
2024-02-19 16:43:33 +0100

    This the second adjusted version of the program that prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,10 +9,12 @@ if __name__ == "__main__":
 
     if num_of_arg == 1:
         print("0 arguments.")
-    elif num_of_arg == 2:
+    
+    if num_of_arg == 2:
         print("{} argument:\n{}: {}".format(1, 1, list_of_arg[1]))
-    elif num_of_arg > 2:
-        print("{} arguments:".format(num_of_arg - 1))  # excluding filename
+    
+    if num_of_arg > 2:
+        print("{} arguments:".format(num_of_arg - 1))
 
         for arg in range(1, num_of_arg):
 
