e2b3fc95d6a08c5efebe2c678756c507abf1bd63
hastrotech1 <hastrotech1@gmail.com>
2024-02-19 17:22:30 +0100

    This program that prints the result of the addition of all arguments (2)

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     import sys
 
-   arg_count = 0
+    arg_count = 0
     arg_len = len(sys.argv) - 1
     for j in range(arg_len):
         arg_count += int(sys.argv[j + 1])
