7feb6e02e814e813b63c7a9a567b02555086878e
hastrotech1 <hastrotech1@gmail.com>
2024-03-07 19:37:41 +0100

    This program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,54 +2,47 @@
 
 from calculator_1 import add, sub, mul, div
 
-
-
-
-
-def arg_calc(argv):
+def calculate(argv):
     
-    n = len(argv) - 1
+    cal_len = len(argv) - 1
     
-    if n != 3:
-        
+    if cal_len != 3:
+
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         
         exit(1)
-        
+
     a = int(argv[1])
-    
-    op = argv[2]
-    
+
+    operat_or = argv[2]
+
     b = int(argv[3])
-    
-    if op == '+':
+
+    if operat_or == '+':
         
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, add(a, b)))
+        print("{:d} {:s} {:d} = {:d}".format(a, operat_or, b, add(a, b)))
         
-    elif op == '-':
+    elif operat_or == '-':
         
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, sub(a, b)))
+        print("{:d} {:s} {:d} = {:d}".format(a, operat_or, b, sub(a, b)))
         
-    elif op == '*':
+    elif operat_or == '*':
         
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, mul(a, b)))
+        print("{:d} {:s} {:d} = {:d}".format(a, operat_or, b, mul(a, b)))
         
-    elif op == '/':
+    elif operat_or == '/':
         
-        print("{:d} {:s} {:d} = {:d}".format(a, op, b, div(a, b)))
+        print("{:d} {:s} {:d} = {:d}".format(a, operat_or, b, div(a, b)))
         
     else:
         
         print("Unknown operator. Available operators: +, -, * and /")
         
         exit(1)
-        
-
-        
 
         
 if __name__ == "__main__":
     
     import sys
     
-    arg_calc(sys.argv)
+    calculate(sys.argv)
