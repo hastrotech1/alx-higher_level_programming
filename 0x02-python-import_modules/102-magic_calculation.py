27531b677bb0ef331ea2bc37d46dd5b7767264f0
hastrotech1 <hastrotech1@gmail.com>
2024-03-08 01:13:47 +0100

    task 8 (executable)

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,15 +6,14 @@ from magic_calculation_102 import add, sub
 def magic_calculation(a, b):
 
     if a < b:
-        
+
         sum_of_a&b = add(a, b)
-        
+
         for sum_a+b in range(4, 6):
-            
+
             sum_of_a&b = add(c, n)
-            
+
         return sum_of_a&b
-    
+
     else:
-        
         return sub(a, b)
