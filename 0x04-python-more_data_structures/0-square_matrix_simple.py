60b9a2cdbe0e250ce8f8d763ca9faaa6e1743ff8
Haastrup Daniel <140532908+hastrotech1@users.noreply.github.com>
2024-05-30 11:07:55 +0100

    used a square loop

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    return [[x**2 for x in row] for row in matrix]
-    
-    # new_matrix = matrix.copy()
-    
 
-    
-    # for i in range(len(matrix)):
-        
-    #     new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-        
+    new_matrix = matrix.copy()
 
-        
-    # return (new_matrix)
+
+    for sq_matrix in range(len(matrix)):
+
+        new_matrix[sq_matrix] = list(map(lambda x: x**2, matrix[sq_matrix]))
+
+    return (new_matrix)
