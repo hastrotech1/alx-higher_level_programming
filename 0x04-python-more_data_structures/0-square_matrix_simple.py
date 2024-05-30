78195d85b0f8fd0257d3e09dfe430dfd7c135feb
Haastrup Daniel <140532908+hastrotech1@users.noreply.github.com>
2024-05-30 10:59:24 +0100

    README, Task-0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    return [[x**2 for x in row] for row in matrix]
+    
+    # new_matrix = matrix.copy()
+    
+
+    
+    # for i in range(len(matrix)):
+        
+    #     new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+        
+
+        
+    # return (new_matrix)
