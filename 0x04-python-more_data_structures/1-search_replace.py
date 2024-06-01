b40378e7f70e7478279393286014614b937e7128
Haastrup Daniel <140532908+hastrotech1@users.noreply.github.com>
2024-06-01 05:13:08 +0100

    line too long

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,7 @@
 
 def search_replace(my_list, search, replace):
 
-    new_list = list(map(lambda _list: replace if _list == search else _list, my_list))
+    new_list = list(map(lambda _list: replace if _list == search
+                        else _list, my_list))
 
     return (new_list)
